2f4e45d958d3886db9dffe40fcf7718e36a74c47
ecgreyes <emanuel.reyes@obf.ateneo.edu>
2022-10-01 21:22:05 +0800

    Added force magnitude calculation draft

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,12 @@
 #include <raylib.h>
 #include <iostream>
+#include <cmath>
 
 int main() {
+    Vector2 cuePos{300.0f, 300.0f};
+    Vector2 initialPos;
+    Vector2 dragPos;
+    float fMagnitude;
 
     InitWindow(800, 600, "Basic Physics and Collision");
 
@@ -10,6 +15,16 @@ int main() {
     SetTargetFPS(targetFPS);
 
     while (!WindowShouldClose()) {
+
+        //Takes initial position on press and position when dragged away and gets the distance.
+        if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+            initialPos = GetMousePosition();
+        }
+        if(IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            dragPos = GetMousePosition();
+        }
+        fMagnitude = sqrt(pow((dragPos.x - initialPos.x), 2) + pow((dragPos.y - initialPos.y), 2));
+
         BeginDrawing();
 
         //Board
@@ -28,7 +43,7 @@ int main() {
         DrawRectangle(750, 125, 50, 350, BROWN); //right
 
         //Cue Ball
-        DrawCircle(300, 300, 30, WHITE);
+        DrawCircleV(cuePos, 30, WHITE);
 
         //Billiard Balls
         DrawCircle(500, 300, 30, BLUE);
