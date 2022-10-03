974274d470386d4e579fd91d1c660d2b6b4bdff7
ecgreyes <emanuel.reyes@obf.ateneo.edu>
2022-10-04 01:16:30 +0800

    Added collision check

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,13 @@ int main() {
     Vector2 initialPos;
     Vector2 dragPos;
     float fMagnitude;
+    float timestep = GetFrameTime(); //multiply to movement
+    Rectangle up = {125, 0, 550, 50};
+    Rectangle down = {125, 550, 550, 50};
+    Rectangle left = {0, 125, 50, 350};
+    Rectangle right = {750, 125, 50, 350};
+    int indicator;
+    
 
     InitWindow(800, 600, "Basic Physics and Collision");
 
@@ -25,6 +32,23 @@ int main() {
         }
         fMagnitude = sqrt(pow((dragPos.x - initialPos.x), 2) + pow((dragPos.y - initialPos.y), 2));
 
+        //cue position
+        cuePos = GetMousePosition();
+
+        //check collision circle-rec
+        if(CheckCollisionCircleRec(cuePos, 50, up)) {
+            indicator = 1;
+        } else if(CheckCollisionCircleRec(cuePos, 50, down)){
+            indicator = 1;
+        } else if(CheckCollisionCircleRec(cuePos, 50, left)){
+            indicator = 1;
+        } else if(CheckCollisionCircleRec(cuePos, 50, right)){
+            indicator = 1;
+        } else {
+            indicator = 0;
+        }
+        
+
         BeginDrawing();
 
         //Board
@@ -37,10 +61,10 @@ int main() {
         DrawCircle(750, 550, 50, BLACK); //lowerright
 
         //Sides
-        DrawRectangle(125, 0, 550, 50, BROWN); //up
-        DrawRectangle(125, 550, 550, 50, BROWN); //down
-        DrawRectangle(0, 125, 50, 350, BROWN); //left
-        DrawRectangle(750, 125, 50, 350, BROWN); //right
+        DrawRectangleRec(up, BROWN); //up
+        DrawRectangleRec(down, BROWN); //down
+        DrawRectangleRec(left, BROWN); //left
+        DrawRectangleRec(right, BROWN); //right
 
         //Cue Ball
         DrawCircleV(cuePos, 30, WHITE);
@@ -50,6 +74,7 @@ int main() {
         DrawCircle(550, 250, 30, BLUE);
         DrawCircle(550, 350, 30, BLUE);
 
+        DrawText(TextFormat("testcollide: %i", indicator), 10, 40, 20, LIGHTGRAY);
         EndDrawing();
     }
     CloseWindow();
